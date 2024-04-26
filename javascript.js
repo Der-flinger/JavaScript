ffde119d979424c78ebd052d5f530d90de7fe8f6
Derflinger <user123@gmail.com>
2024-04-26 13:43:15 +0300

    task Seminar 1

---
+++ javascript.js
@@ -0,0 +1,37 @@
+/* console.log("текст из javascript");
+let num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a); */
+
+/* let a = 123;
+let b = a + 4;
+console.log(b); */
+
+/* let a = 4;
+let b = 12;
+
+let sum = a + b;
+let difference = a - b;
+let multiply = a * b;
+let divide = a / b;
+
+console.log(sum);
+console.log(difference);
+console.log(multiply);
+console.log(divide);
+console.log("----------------");
+console.log(`total: ${sum + difference + multiply + divide}`); */
+
+/* let x = 0.454;
+let y = 1.201;
+console.log(x + y); */
+
+// let c = 543;
+// console.log(-c);
+
+/* let name = "Johny";
+let surname = "Silverhand";
+console.log(`${name} ${surname}`); */
