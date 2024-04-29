58e45cb1b67e47ed73a0237bc5ed498b9201c800
Derflinger <user123@gmail.com>
2024-04-29 08:51:59 +0300

    Added Seminar 3 tasks to submit

--- Seminar3.js
+++ Seminar3.js
@@ -1,30 +1,3 @@
-/* const array = [15453, 221, -282, 43, "a", "gfg", "CAZALUT"];
-for (let i = 0; i < array.length; i++) {
-	console.log(array[i]);
-}
-array[5] = -555555;
-console.log(array);
-array.splice(3, 1);
-console.log(array);
-console.log("lenght of array =", array.length);
- */
-
-/* const arr = [];
-for (let i = 11; i < 34; i++) {
-	arr[i - 11] = i;
-}
-console.log(arr);
-console.log("lenght of array =", arr.length); */
-
-/* const arr = [];
-for (let i = 0; i < 100; i++) {
-	if (i % 2 !== 0) {
-		arr.push(i);
-	}
-}
-console.log(arr);
-console.log("lenght of array =", arr.length); */
-
 /* let num = prompt("Enter the number");
 console.log(`multiply number ${num} for 3`);
 let count = 0;
@@ -38,3 +11,59 @@ while (num < 1000) {
 }
 console.log(`result value = ${num} lesser 1000`);
 console.log(`count of iteration - ${count}`); */
+
+/*Задание 1
+	Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
+function cubing(num) {
+	return num ** 3;
+}
+console.log(`Ответ: ${cubing(prompt("Введите число для возведения в куб"))}`); */
+
+/*Задание 2
+Пользователь вводит с клавиатуры число, если ввёл текст, необходимо вывести что значение задано неверно
+Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
+
+ function salaryCalc(salary) {
+	if (typeof salary == "number") {
+		console.log(
+			`Размер заработной платы за вычетом налогов равен ${Math.floor(salary * 0.87)}`
+		);
+	} else console.log("Некорректный тип данных");
+}
+salaryCalc("34300"); */
+
+/* Задание 3
+Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел
+function maxAmongThree(num1, num2, num3) {
+	console.log(`The first number equal ${num1}`);
+	console.log(`The second number equal ${num2}`);
+	console.log(`The third number equal ${num3}`);
+	console.log("------------------------------------");
+	return Math.max(num1, num2, num3);
+}
+console.log(
+	`Максимальное число среди трех равно ${maxAmongThree(
+		prompt("Введите первое число"),
+		prompt("Введите второе число"),
+		prompt("Введите третье число")
+	)}`
+) */
+
+// Задание 4
+// Необходимо реализовать четыре функции, каждая функция должна принимать по два числа и выполнять одну из операций (каждая функция выполняет одну из них):
+// 1. Сложение
+// 2. Разность
+// 3. Умножение
+// 4. Деление
+// Необходимо сделать так, чтобы функция вернула число, например выражение console.log(sum(2, 6)); должно вывести число 8 в консоль (sum - функция сложения в данном примере, ваши названия функций могут отличаться). Округлять значения, которые возвращают функции не нужно, однако, обратите внимание на разность, функция должна вычесть из большего числа меньшее, либо вернуть 0, если числа равны. Функциям всегда передаются корректные числа, проверки на NaN, Infinity делать не нужно.
+const sum = (num1, num2) => num1 + num2;
+const difference = (num1, num2) => {
+	if (num1 > num2) return num1 - num2;
+	else return num2 - num1;
+};
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+console.log(`The sum of the numbers = ${sum(23, 43)}`);
+console.log(`The difference of the numbers = ${difference(51, 42)}`);
+console.log(`The multiply of the numbers = ${multiply(10, 48)}`);
+console.log(`The divide of the numbers = ${divide(65, 23)}`);
